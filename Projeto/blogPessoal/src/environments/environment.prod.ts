12c42fec3d4902b20c47922daea5819c8d2f9030
Kathleen-Fidelis <kathleenfidelis12@gmail.com>
2021-05-31 18:55:49 -0300

    Consumo de API - Login e Cadastro

--- Projeto/blogPessoal/src/environments/environment.prod.ts
+++ Projeto/blogPessoal/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  token: '',
+  nome: '',
+  id: 0,
+  foto: '',
+  server: 'https://blogpessoalkathleen.herokuapp.com'
 };
+
+//https://blogpessoalkathleen.herokuapp.com/
+//http://localhost:8080
